14bc14435da27c4f8b5525743b3bc6203feadca3
dragox134 <legi1810@gmail.com>
2022-07-26 16:25:11 +0200

    import from last project state mashine

---
+++ Assets/Scripts/PlayerCombat.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCombat : MonoBehaviour
+{
+    public Animator animator;
+
+    public Transform attackPoint;
+    public float attackRange;
+    public LayerMask enemyLayer;
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            animator.SetTrigger("Attack");
+        }
+    }
+
+    public void EnemyDetection()
+    {
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayer);
+
+        foreach (Collider2D enemy in hitEnemies)
+        {
+            Debug.Log("We hit " + enemy.name);
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        if (attackPoint == null)
+            return;
+        Gizmos.DrawWireSphere(attackPoint.position, attackRange); 
+    }
+}
+
+
