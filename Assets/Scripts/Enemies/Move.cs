6d6a2240304629ebdc59d8c46799723eabaaa8fb
dragox134 <legi1810@gmail.com>
2022-07-27 23:04:10 +0200

    enemy jumping (not finished)

--- Assets/Scripts/Enemies/Move.cs
+++ Assets/Scripts/Enemies/Move.cs
@@ -5,22 +5,33 @@ using UnityEngine;
 public class Move : State
 {
     Player player;
+
+    float jumpForce;
+    bool grounded;
     public override void Enter()
     {
         Debug.Log("Enter Move");
+        Delay();
     }
+    IEnumerator Delay()
+    {
 
+        yield return new WaitForSeconds(Random.Range(1, 5));
+        yield return Delay();
+    }
     public override void Process()
     {
         float direction = enemy.transform.position.x - player.transform.position.x;
 
-        Debug.Log(direction);
         enemy.rb.velocity = new Vector3(direction, 0).normalized * enemy.speed;
+
     }
 
     public Move(EnemyBase enemy, Player player) : base(enemy)
     {
         this.player = player;
+        this.jumpForce = enemy.jumpForce;
+        this.grounded = enemy.grounded;
     }
 
     public override void Exit()
