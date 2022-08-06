814ea3204017fff39ba10efa48dc8cf85c3b8387
dragox134 <legi1810@gmail.com>
2022-08-06 19:20:01 +0200

    player nehituje cez pol mapy

---
+++ Assets/Scripts/SquidEnemy/EmptyObjectScript.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EmptyObjectScript : MonoBehaviour
+{
+    private SquidBase enemy;
+
+    private void Start()
+    {
+        enemy = GetComponentInParent<SquidBase>();
+    }
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            enemy.TriggerEnter(collision.GetComponent<Player>());
+        }
+    }
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            enemy.TriggerExit();
+        }
+    }
+}
