4068a9369fbadcd3a164e19b9f6d4876b2b522e4
Kosorin <koshiii333@gmail.com>
2022-08-08 13:22:41 +0200

    New Attack System

--- Assets/Scripts/Player/PlayerCombat.cs
+++ Assets/Scripts/Player/PlayerCombat.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -12,84 +13,55 @@ public class PlayerCombat : MonoBehaviour
 
     public Transform attackPoint;
     public float attackRange;
-    public int attackDemage;
+    public int attackDamage;
     public LayerMask enemyLayer;
 
     public bool attacking;
+    public float attackDelay;
 
     void Start()
     {
         player = this.transform;
         anim = GetComponent<Animator>();
-        ammo = maxAmmo;
     }
 
-    public GameObject[] ammoPoints;
-    public int ammo;
-    public int maxAmmo;
-    public bool reload;
-
-    private float time = 1.2f;
-    private bool relode = false;
-
-    public 
-    void Update()
+    private void Update()
     {
 
-        for (int i = 0; i < ammoPoints.Length; i++)
-        {
-            ammoPoints[i].SetActive(i < ammo);
-        }
-
-
-
-        if (relode)
-        {
-            time -= Time.deltaTime;
-        }
-
-        if (time <= 0)
-        {
-
-
-            if (ammo == maxAmmo)
-            {
-                relode = false;
-            }
-            time = 1.2f;
-            ammo++;
-        }
     }
-
-
 
     public void EnemyDetection(InputAction.CallbackContext context)
     {
-        if (context.performed && ammo > 0 && player.GetComponent<PlayerInteract>().ItemSlot.IsFull() == false)
+        if (context.performed && !attacking)
         {
-            attacking = true;
-            Debug.Log("You see");
-            ammo--;
-            anim.SetTrigger("Attack");
-            Hit();
-            relode = false;
-            relode = true;
-            time = 1.2f;
+            StartCoroutine(Attack());;
         }
+    }
+
+    private IEnumerator Attack()
+    {
+        anim.SetTrigger("Attack");
+        Hit();
+        attacking = true;
+        yield return new WaitForSeconds(attackDelay);
+        attacking = false;
     }
 
     public void Hit()
     {
-
         Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayer);
 
         foreach (Collider2D enemy in hitEnemies)
         {
-            Debug.Log("We hit " + enemy.name);
-            enemy.GetComponent<SquidBase>().Demaged(attackDemage, player);
-            
+            var squidBase = enemy.GetComponent<SquidBase>();
+
+            if (squidBase == null)
+            {
+                continue;
+            }
+
+            squidBase.Demaged(attackDamage, player);
         }
-        attacking = false;
     }
 
     private void OnDrawGizmosSelected()
