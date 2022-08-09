88b83d7399671f4e02d89fd328794a8e4c622b48
Adam Woska <ademwoska@gmail.com>
2022-08-09 15:44:21 +0200

    Squid fix

--- Assets/Scripts/SquidEnemy/SquidScript.cs
+++ Assets/Scripts/SquidEnemy/SquidScript.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -14,21 +15,42 @@ public class SquidScript : MonoBehaviour
 
     public bool right = true;
     public float direction = 1;
+    [SerializeField] private LayerMask ground;
+
+
+
+    [SerializeField] private Item deadEnemyItem;
+    [SerializeField] private ItemObject itemObjectPrefab;
+
+    private void Start()
+    {
+        Death();
+    }
+
     private void Update()
     {
         
-        RaycastHit2D groundInfo = Physics2D.Raycast(groundDetector.position, Vector2.down, 2f, LayerMask.NameToLayer("Ground"));
-        RaycastHit2D wallInfo = Physics2D.Raycast(transform.position, Vector2.right, 10f, LayerMask.NameToLayer("Ground"));
-        rb.velocity = new Vector2(transform.right.x, rb.velocity.y) * speed;
-        
-        if (/*groundInfo.collider == false || */wallInfo.collider != null)
+        //RaycastHit2D groundInfo = Physics2D.Raycast(groundDetector.position, Vector2.down, 2f, LayerMask.NameToLayer("Ground"));
+        RaycastHit2D wallInfo = Physics2D.Raycast(transform.position, transform.right, 1.5f, ground);
+        Debug.DrawRay(transform.position, transform.right, Color.red);
+        if (wallInfo.collider != null)
         {
             transform.right *= -1;
+            Debug.Log(("koliyia"));
         }
+        rb.velocity = new Vector2(transform.right.x, rb.velocity.y) * speed;
     }
     private void OnDrawGizmos()
     {
         Gizmos.DrawLine(transform.position, transform.position + transform.right * 10);
+    }
+
+    private void Death()
+    {
+        var itemObject = Instantiate(itemObjectPrefab, transform.position, Quaternion.identity);
+                
+        itemObject.Init(deadEnemyItem);
+        Destroy(gameObject);
     }
 
 
